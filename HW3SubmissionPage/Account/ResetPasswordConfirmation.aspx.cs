2425ab1901c86d766b1e28c4c47d2bd42f3550ce
John Robertson <jj.robertson@asu.edu>
2014-11-08 11:13:49 -0800

    Web application created.  Initial commit.

---
+++ HW3SubmissionPage/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace HW3SubmissionPage.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ HW3SubmissionPage/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HW3SubmissionPage.Startup))]
+namespace HW3SubmissionPage
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
